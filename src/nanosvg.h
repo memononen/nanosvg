26f57d198146216184e71e254f8e8d2bf7da02ed
Malcolm McLean <malcolm.mclean5@btinternet.com>
2016-08-31 08:10:14 +0100

    Binary search on colour names

--- src/nanosvg.h
+++ src/nanosvg.h
@@ -1322,10 +1322,30 @@ NSVGNamedColor nsvg__colors[] = {
 static unsigned int nsvg__parseColorName(const char* str)
 {
 	int i, ncolors = sizeof(nsvg__colors) / sizeof(NSVGNamedColor);
-
-	for (i = 0; i < ncolors; i++) {
+	
+	for (i = 0; i < 10; i++) {
 		if (strcmp(nsvg__colors[i].name, str) == 0) {
 			return nsvg__colors[i].color;
+		}
+	}
+	if (ncolors > 10)
+	{
+		int low, high, med;
+		int res;
+		low = 10;
+		high = ncolors - 1;
+
+		while (low <= high)
+		{
+			med = (low + high) / 2;
+			res = strcmp(nsvg__colors[med].name, str);
+			if(res < 0)
+				low = med + 1;
+			else if (res > 0)
+				high = med - 1;
+			else
+				return nsvg__colors[med].color;
+
 		}
 	}
 
