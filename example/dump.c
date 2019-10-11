83c8178369d39286ce4d1bb7ed26fa9bbdca819d
michael tesch <tesch1@gmail.com>
2019-10-11 11:47:02 +0200

    tabs formatting

--- example/dump.c
+++ example/dump.c
@@ -27,39 +27,39 @@ NSVGimage* g_image = NULL;
 
 int main(int argc, char *argv[])
 {
-  for (int arg = 1; arg < argc; arg++) {
-    const char* filename = argv[arg];
+	for (int arg = 1; arg < argc; arg++) {
+		const char* filename = argv[arg];
 
-    g_image = nsvgParseFromFile(filename, "px", 96.0f);
-    if (g_image == NULL) {
-      printf("Could not open SVG image '%s'.\n", filename);
-      return -1;
-    }
+		g_image = nsvgParseFromFile(filename, "px", 96.0f);
+		if (g_image == NULL) {
+			printf("Could not open SVG image '%s'.\n", filename);
+			return -1;
+		}
 
-    printf("%s:\n", filename);
-    printf("size: %f x %f.\n", g_image->width, g_image->height);
+		printf("%s:\n", filename);
+		printf("size: %f x %f.\n", g_image->width, g_image->height);
 
 #if 0
-    for (NSVGgroup* group = g_image->groups; group != NULL; group = group->next) {
-      printf("group: %s parent:%s\n", group->id, group->parent ? group->parent->id : "-");
-    }
+		for (NSVGgroup* group = g_image->groups; group != NULL; group = group->next) {
+			printf("group: %s parent:%s\n", group->id, group->parent ? group->parent->id : "-");
+		}
 #endif
 
-    for (NSVGshape* shape = g_image->shapes; shape != NULL; shape = shape->next) {
-      printf("shape: '%s' visible:%d\n", shape->id, 0 != (shape->flags & NSVG_FLAGS_VISIBLE));
+		for (NSVGshape* shape = g_image->shapes; shape != NULL; shape = shape->next) {
+			printf("shape: '%s' visible:%d\n", shape->id, 0 != (shape->flags & NSVG_FLAGS_VISIBLE));
 #if 0
-      if (shape->group)
-        printf("     : group '%s'\n", shape->group->id);
+			if (shape->group)
+				printf("     : group '%s'\n", shape->group->id);
 #endif
-      for (NSVGpath* path = shape->paths; path != NULL; path = path->next) {
-        //drawPath(path->pts, path->npts, path->closed, px * 1.5f);
-        printf(" npts: %d  [%f %f %f %f]\n", path->npts,
-               path->bounds[0], path->bounds[1], path->bounds[2], path->bounds[3]);
-      }
-    }
+			for (NSVGpath* path = shape->paths; path != NULL; path = path->next) {
+				//drawPath(path->pts, path->npts, path->closed, px * 1.5f);
+				printf(" npts: %d [f:%d] [%f %f %f %f]\n", path->npts, 0,
+					   path->bounds[0], path->bounds[1], path->bounds[2], path->bounds[3]);
+			}
+		}
 
-    nsvgDelete(g_image);
-  }
+		nsvgDelete(g_image);
+	}
 
-  return 0;
+	return 0;
 }
